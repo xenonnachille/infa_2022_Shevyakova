46a5b23b8b6393f6a5d60e050f96c6760d03d7a8
xenonnachille <ksenia-sherlock@yandex.ru>
2022-10-31 10:44:21 +0300

    draw_1

--- lab8/1_draw.py
+++ lab8/1_draw.py
@@ -5,20 +5,24 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
+screen.fill("white")
+pygame.display.update()
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+
+color_yellow = (255, 255, 0)
+color_black = (0, 0, 0)
+circle(screen, color_yellow, (200, 175), 50)
+circle(screen, color_black, (200, 175), 50, 1)
+rect(screen, (5, 44, 150), (150, 150, 40, 5))
+rect(screen, (5, 44, 150), (215, 150, 40, 5))
+
+
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
-finished = False
 
+finished = False
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
